4c5a25d60f2746f42c958616180ee820cbb2b827
Jack Weinbender <jack.weinbender@gmail.com>
2018-03-09 16:03:39 -0600

    Good compile

--- build.js
+++ build.js
@@ -1,17 +1,37 @@
 var fs = require('fs');
 const hbs = require('handlebars')
 
+// Load Data from JSON
 const letters = require('./letters')
 const pages = require('./pages')
+const roots = require('./roots')
 
-fs.readFile('templates/index.hbs', 'utf8', function(err, contents) {
-    if (err) throw err;
-    let t = hbs.compile(contents)
-    pages.forEach(page => {
-        let output = t({letters: letters, page: page})
-        fs.writeFile(`dist/${page.id}.html`, output, (err) => {
-            if (err) throw err
-        });
-    })
-});
+// Load Template and Partial Files
+const index     = fs.readFileSync('index.hbs', 'utf8')
+const head      = fs.readFileSync('partials/head.hbs', 'utf8')
+const nav       = fs.readFileSync('partials/nav.hbs', 'utf8')
+const page_nav  = fs.readFileSync('partials/page-nav.hbs', 'utf8')
+
+// Precompile the Navigateion (cuts compile in half)
+let n = hbs.compile(nav)
+const nav_compiled = n({roots:roots, letters:letters})
+
+// Register Partials
+hbs.registerPartial('head', head)
+hbs.registerPartial('page-nav', page_nav)
+hbs.registerPartial('nav', nav_compiled)
+
+// Compile the main template
+let template = hbs.compile(index)
+
+// Write out Each Page
+pages.forEach(page => {
+    let data = { page: page, letters: letters, roots: roots }
+    let output = template(data)
+    fs.mkdirSync(`dist/page/${page.id}/`)
+
+    fs.writeFile(`dist/page/${page.id}/index.html`, output, (err) => {
+        if (err) throw err
+    });
+})
 
