c5d41bc560f5579ee0b4d9f49915773c84942e6a
Jack Weinbender <jack.weinbender@gmail.com>
2018-03-09 11:19:19 -0600

    Update. Buggy build

--- build.js
+++ build.js
@@ -1,7 +1,17 @@
+var fs = require('fs');
 const hbs = require('handlebars')
 
 const letters = require('./letters')
 const pages = require('./pages')
 
-console.log(letters)
+fs.readFile('templates/index.hbs', 'utf8', function(err, contents) {
+    if (err) throw err;
+    let t = hbs.compile(contents)
+    pages.forEach(page => {
+        let output = t({letters: letters, page: page})
+        fs.writeFile(`dist/${page.id}.html`, output, (err) => {
+            if (err) throw err
+        });
+    })
+});
 
