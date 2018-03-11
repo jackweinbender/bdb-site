b2c5a24acaa65d0ac5799e2573bb2de05a9725f6
Jack Weinbender <jack.weinbender@gmail.com>
2018-03-11 09:12:53 -0500

    Reformatted url structure for simplicity

--- build.js
+++ build.js
@@ -24,14 +24,6 @@ hbs.registerPartial('nav', nav_compiled)
 // Compile the main template
 let template = hbs.compile(index)
 
-
-// Build the Dictionary Structure
-fs.mkdirSync(`dist/preface/`)
-fs.mkdirSync(`dist/abbr/`)
-fs.mkdirSync(`dist/hebrew/`)
-fs.mkdirSync(`dist/aramaic/`)
-fs.mkdirSync(`dist/errata/`)
-
 // Write out Each Page to its Section
 pages.forEach(page => {
     let data = { page: page, letters: letters, roots: roots }
@@ -41,8 +33,7 @@ pages.forEach(page => {
     if(page.section == 'title'){
         fs.writeFileSync(`dist/index.html`, output)
     } else {
-        fs.mkdirSync(`dist/${page.section}/${page.id}/`)
-        fs.writeFileSync(`dist/${page.section}/${page.id}/index.html`, output)
+        fs.writeFileSync(`dist/${page.section}-${page.id}.html`, output)
     }
 })
 
--- get_images.py
+++ get_images.py
@@ -1,5 +1,6 @@
 import wget
 import json
+from pathlib import Path
 
 with open('_data/pages.json', 'r') as pages_file:
     pages = json.load(pages_file)
@@ -8,6 +9,9 @@ with open('_data/pages.json', 'r') as pages_file:
         emory = page['emory_page']
         section = page['section']
         pid = page['id']
-        url = f'http://iiif.archivelab.org/iiif/00825376.1523.emory.edu${emory}/full/1024/0/gray.png'
-        print(f'\nDownloading: {section}: {pid} @ \n\t{url}')
-        wget.download(url, f'images/{section}_{pid}.png')
+        f = Path(f'images/{section}_{pid}.png')
+        if f.is_file(): continue
+        else:
+            url = f'http://iiif.archivelab.org/iiif/00825376.1523.emory.edu${emory}/full/1024/0/gray.png'
+            print(f'\nDownloading: {section}: {pid} @ \n\t{url}')
+            wget.download(url, f'images/{section}_{pid}.png')
